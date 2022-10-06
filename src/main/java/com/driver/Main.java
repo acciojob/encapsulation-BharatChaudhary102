45b466effd87208bf48646a061c6a6a7f50ab8c0
Bharat1002 <bharatjc1002@gmail.com>
2022-10-06 18:34:57 +0530

    task

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly ob = new RWOnly();
+
+//        ob.name = "Private var";
+//        System.out.println(ob.name);
+        // Error: :8:11 java: name has private access in com.driver.RWOnly
+
+        ob.setter("New Name");
+        System.out.println(ob.getter());
+    }
 }
